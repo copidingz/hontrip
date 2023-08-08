a4dab9b6b4933258dfb0c19183cbb485c569c813
yun923 <kkee2512@naver.com>
2023-08-08 15:50:19 +0900

    [FEAT] planController insert문 수정

--- src/main/java/com/multi/hontrip/plan/controller/PlanController.java
+++ src/main/java/com/multi/hontrip/plan/controller/PlanController.java
@@ -3,6 +3,6 @@ package com.multi.hontrip.plan.controller;
 // 컨트롤러입니다.
 public class PlanController {
     public void inset() {
-        System.out.println("테스트입니다2");
+        System.out.println("테스트입니다22");
         }
 }
